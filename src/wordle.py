0c7754b4b0656578065840a8caff7f445c3879c1
sneha_dubey <yourselflove9760@gmail.com>
2025-11-06 18:05:30 +0530

    Update wordle.py

--- src/wordle.py
+++ src/wordle.py
@@ -1,1 +1,7 @@
+if __name__ == "__main__":
+    console = Console()
+    chosen_word = choice(word_list)
+    console.print(WELCOME_MESSAGE)
+    console.print(PLAYER_INSTRUCTIONS)
+    game(console, chosen_word)
 
